78ce7c20133e764d40f9204cb7a1edd06356e722
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-12 12:38:06 +0530

    Update Auth.js

--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './supabaseClient';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
