579c80cef7dcc1ef8f6ed1e021f302ff63933f5f
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-13 11:05:26 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// File: src/App.tsx
+import React from 'react'
+import DefectsTable from './components/DefectsTable'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#132337] p-4">
+      <DefectsTable />
+    </div>
+  )
+}
+
+export default App
