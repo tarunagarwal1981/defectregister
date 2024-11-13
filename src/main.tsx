fe7a6ce8eb3c96da42395a4a5c1dc05a8d6e3e68
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-13 11:56:31 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+// src/main.tsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
